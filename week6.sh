989db035694c21506e826e36b6fe5685f16660c8
dawk42 <derek.vanvuuren@gmail.com>
2020-05-18 19:50:05 -0700

    setup for local

--- week6.sh
+++ week6.sh
@@ -3,5 +3,3 @@ ansible -i host.ini web -m yum -a "name=httpd state=latest update_cache=yes" -b
 ansible -i host.ini web -m service -a "name=httpd enabled=yes state=started" -b
 ansible -i host.ini db -m yum -a "name=mariadb-server state=latest update_cache=yes" -b
 ansible -i host.ini db -m service -a "name=mariadb enabled=yes state=started" -b
-ansible -i host.ini db -m shell -a 'systemctl status mariadb' -b
-firefox 192.168.0.111
